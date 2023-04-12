ffc3dee3f10fd0294e052801c88581fb06d87bff
william <williar2519@gmail.com>
2023-04-12 18:00:26 -0500

    3.commit

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -1,13 +1,32 @@
 package com.fundamentosplatzi.springboot.fundamentos;
 
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyImplementWithDependency;
+import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class FundamentosApplication {
-
+public class FundamentosApplication implements CommandLineRunner {
+	private ComponentDependency componentDependency;
+	private MyBean myBean;
+	private MyImplementWithDependency myImplementWithDependency;
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyImplementWithDependency myImplementWithDependency){
+		this.componentDependency = componentDependency;
+		this.myBean = myBean;
+		this.myImplementWithDependency = myImplementWithDependency;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		componentDependency.saludar();
+		myBean.print();
+		myImplementWithDependency.withDependenci();
+	}
 }
---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/component/ComponentImplement.java
@@ -0,0 +1,10 @@
+package com.fundamentosplatzi.springboot.fundamentos.component;
+
+import org.springframework.stereotype.Component;
+@Component
+public class ComponentImplement implements ComponentDependency {
+    @Override
+    public void saludar() {
+        System.out.println("Hello world since my component");
+    }
+}
